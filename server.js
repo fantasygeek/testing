65383f6fe813d2979fd43f920f87728f70723a15
se17thjb <sethjacobbb@gmail.com>
2025-08-19 16:56:25 +0800

    test

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
-const { createServer } = require('http')
-const { parse } = require('url')
-const next = require('next')
+const express = require('express');
+const next = require('next');
 
-const dev = process.env.NODE_ENV !== 'production'
-const hostname = process.env.WEBSITE_HOSTNAME || 'localhost'
-const port = process.env.PORT || 8080
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-// when using middleware `hostname` and `port` must be provided below
-const app = next({ dev, hostname, port })
-const handle = app.getRequestHandler()
+// Your app will get the Azure port from the process.enc.PORT
+const port = process.env.PORT || 3000;
 
-app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
-      const parsedUrl = parse(req.url, true)
-      const { pathname, query } = parsedUrl
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-      await handle(req, res, parsedUrl)
-    } catch (err) {
-      console.error('Error occurred handling', req.url, err)
-      res.statusCode = 500
-      res.end('internal server error')
-    }
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log('> Ready on http://localhost:3000');
+    });
   })
-    .once('error', (err) => {
-      console.error(err)
-      process.exit(1)
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`)
-    })
-})
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
